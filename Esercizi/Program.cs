57cc68cd587476f993d141f5c2f5984c937d1f2f
Elisabetta Altea <altea.elisabetta@gmail.com>
2021-06-29 16:13:03 +0200

    esercizio 3

--- Esercizi/Program.cs
+++ Esercizi/Program.cs
@@ -6,6 +6,7 @@ namespace Esercizi
     {
         static void Main(string[] args)
         {
+            //Esercizio1
             int c = 0;
             do
             {
@@ -41,6 +42,53 @@ namespace Esercizi
                 c = int.Parse(Console.ReadLine());
             }
             while (c == 0);
+
+            //Esercizio2
+            int cont = 0;
+            string s = "mattinata";
+            char l = 'a';
+
+            for (int index = 0; index < s.Length; index++)
+            {
+                if (s[index] == l)
+                {
+                    ++cont;
+                }
+            }
+            Console.WriteLine($"La lettera {l} compare {cont} volte nella stringa \"mattinata\"!");
+
+            //Esercizio3
+            int[] arrayOrd = new int[5];
+            int[] array = { 3, 6, 2, 7, 8 };
+
+            for (int ind = 0; ind < array.Length; ind++)
+            {
+                Console.WriteLine(array[ind]);
+            }
+
+            //Console.WriteLine($"L'elemento in posizione 1 è {array[0]}");
+            //Console.WriteLine($"L'elemento in posizione 3 è {array[2]}");
+
+            for (int i=1; i<5; i++)
+            {
+                for (int j=1; j<=i; j++)
+                    if (array[j-1]<array[j])
+                    {
+                        arrayOrd[j-1] = array[j-1];
+                        arrayOrd[j] = array[j];
+                    }
+                    else
+                    {
+                        arrayOrd[j-1] = array[j];
+                        arrayOrd[j] = array[j-1];
+                    }
+            }
+
+            for (int ind = 0; ind < arrayOrd.Length; ind++)
+            {
+                Console.WriteLine(arrayOrd[ind]);
+            }
+
         }
     }
 }
