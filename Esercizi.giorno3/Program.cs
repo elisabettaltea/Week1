f6c61fc830d22ce6b9c644a1b3043f2d1deadd7b
Elisabetta Altea <altea.elisabetta@gmail.com>
2021-07-01 11:31:15 +0200

    esercizio dadi

--- Esercizi.giorno3/Program.cs
+++ Esercizi.giorno3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Esercizi.giorno3
 {
@@ -6,6 +8,8 @@ namespace Esercizi.giorno3
     {
         static void Main(string[] args)
         {
+            ////Esercizio 0
+
             //int[] c = {1, 20, 35, 7, 40};
             //Console.WriteLine("Inserisci l'elemento da cercare");
             //int n = int.Parse(Console.ReadLine());
@@ -32,44 +36,100 @@ namespace Esercizi.giorno3
             //    }
             //}
 
-            //Scrivere una routine InsertNumbers che chiede all'utente di inserire 3 numeri interi diversi da 0
-            //e li salva in un array e me lo restituisce.
-            //Verificare che i numeri aggiunti dall'utente siano del formato corretto
-            //e verificare che siano diversi da 0 con un'altra routine CheckIns, da chiamare in InsertNumbers.
 
 
-            int[] array = new int[3];
-            InsertNumber(ref array);
+            ////Esercizio 1
+            ////Scrivere una routine InsertNumbers che chiede all'utente di inserire 3 numeri interi diversi da 0
+            ////e li salva in un array e me lo restituisce.
+            ////Verificare che i numeri aggiunti dall'utente siano del formato corretto
+            ////e verificare che siano diversi da 0 con un'altra routine CheckIns, da chiamare in InsertNumbers.
 
-            Console.WriteLine();
-            Console.WriteLine("Il tuo vettore formato da 3 numeri interi diversi da zero è ");
-            foreach (int i in array)
+
+            //int[] array = new int[3];
+            //InsertNumber(ref array);
+
+            //Console.WriteLine();
+            //Console.WriteLine("Il tuo vettore formato da 3 numeri interi diversi da zero è ");
+            //foreach (int i in array)
+            //{
+            //    Console.WriteLine($"{i} ");
+            //}
+
+            //void InsertNumber(ref int[] a)
+            //{
+
+            //    for (int i=0; i<3; i++)
+            //    {
+            //        Console.WriteLine($"Inserisci l'elemento {i}-esimo del vettore:");
+            //        int s = CheckIns(int.TryParse(Console.ReadLine(), out int result), result);
+            //        a[i] = s;
+            //    }
+
+            //}
+
+            //int CheckIns(bool verifica, int result)
+            //{
+            //    while (!(verifica && result!=0))
+            //    {
+            //        Console.WriteLine("Valore inserito errato, riprova! Inserisci un numero intero diverso da 0:");
+            //        verifica = int.TryParse(Console.ReadLine(), out int res);
+            //        result = res;
+            //    }
+            //    return result;    
+            //}
+
+
+
+            //Esercizio2
+
+            //Scrivere una routine che, dati due array, verifichi quali sono gli elementi in comune,
+            //ovvero presenti in entrambi gli array, e stampare solo gli elementi in comune. 
+            //Se non ci sono elementi in comune, stampare 'Non ci sono elementi in comune'.
+
+            int[] a1 = { 1, 2, 3 };
+
+            //int[] b1 = { 4, 7, 9 };
+            int[] b1 = { 4, 2, 9 };
+            //int[] b1 = { 4, 2, 1 };
+
+            cerca(a1, b1);
+
+            void cerca(int[] a, int[] b)
             {
-                Console.WriteLine($"{i} ");
-            }
+                ArrayList inComune = new ArrayList() { };
 
-            void InsertNumber(ref int[] a)
-            {
-               
-                for (int i=0; i<3; i++)
+                for (int i = 0; i < a.Length; i++)
                 {
-                    Console.WriteLine($"Inserisci l'elemento {i}-esimo del vettore:");
-                    int s = CheckIns(int.TryParse(Console.ReadLine(), out int result), result);
-                    a[i] = s;
+                    for (int j = 0; j < b.Length; j++)
+                    {
+                        if (a[i] == b[j])
+                        {
+                            inComune.Add(a[i]);
+                        }
+
+                    }
                 }
-                
+
+                if (inComune.Count > 0)
+                {
+                    Console.WriteLine("Gli elementi in comune fra i due vettori sono:");
+                    foreach (int i in inComune)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+                else
+                {
+                    Console.Write("I due vettori non hanno elementi in comune!");
+                }
             }
 
-            int CheckIns(bool verifica, int result)
-            {
-                while (!(verifica && result!=0))
-                {
-                    Console.WriteLine("Valore inserito errato, riprova! Inserisci un numero intero diverso da 0:");
-                    verifica = int.TryParse(Console.ReadLine(), out int res);
-                    result = res;
-                }
-                return result;    
-            }
+
+
+            //Esercizio 3
+            //Scrivere una routine che chiede all'utente di scrivere i giorni della settimana (Es. Lun Mar Mer Gio Ven Sab Dom).
+            //Se l'utente ha già inserito un giorno, stampare 'Hai già inserito questo giorno' e far inserire  nuovo.
+            //Infine, stampare i giorni nell'ordine in cui l'utente li ha inseriti. 
         }
     }
 }
---
+++ Esercizi.giorno4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Esercizi.giorno4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esercizio
+            //Scrivere un programma completo. L'utente inserisce un numero tra 2 e 12.
+            //Il computer lancia 2 dadi, quindi sorteggia 2 numeri random tra 1 e 6.
+            //Se la somma dei due numeri random è pari al numero scelto dall'utente, l'utente vince
+            //Se l'utente vince, stampare 'hai vinto', altrimenti 'hai perso'.
+            //Finita la partita l'utente deve poter rigiocare.
+            //Requisiti: verificare che l'utente inserisca un intero e che sia compreso tra 2 e 12.
+            //Se la verifica non va a buon fine, l'utente deve potere inserire nuovamente qualcosa.
+            //Suggerimento: creare un metodo per l'inserimento dei numeri e la verifica,
+            //uno per il controllo della vittoria(da chiamare nel main).
+
+
+
+            do
+            {
+                tentativo(out int number, out int sum);
+
+                verificaVittoria(number, sum);
+
+                Console.WriteLine("Vuoi continuare? s per si");
+                
+            }
+            while (Console.ReadKey().KeyChar == 's');
+
+
+            void tentativo(out int number, out int sum)
+            {
+                Console.WriteLine("Inserisci un numero tra 2 e 12:");
+
+                number = CheckIns();
+                //Console.WriteLine($"Hai inserito il numero {number}!");
+
+                Random random = new Random();
+                //int randomNum = random.Next(2, 13);
+                //Console.WriteLine($"Il numero sorteggiato è {randomNum}!");
+                int randomNum1 = random.Next(1, 7);
+                //Console.WriteLine($"Il primo numero sorteggiato è {randomNum1}!");
+                int randomNum2 = random.Next(1, 7);
+                //Console.WriteLine($"Il secondo numero sorteggiato è {randomNum2}!");
+                sum = randomNum1 + randomNum2;
+                //Console.WriteLine($"La somma dei numeri sorteggiati è {sum}!");
+
+            }
+
+
+            int CheckIns()
+            {
+                int num;
+                while (!int.TryParse(Console.ReadLine(), out num) || num < 2 || num > 12)
+                {
+                    Console.WriteLine("Puoi inserire solo interi compresi tra 2 e 12! Riprova:");
+                }
+
+                return num;
+            }
+
+
+            void verificaVittoria(int number, int sum)
+            {
+                if (number == sum)
+                {
+                    Console.WriteLine("Hai vinto!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Hai perso :(");
+                }
+            }
+
+        }
+    }
+}
